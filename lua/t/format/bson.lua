da4f00aea7dbb2a18f01f8fcc71a302d994aed71
luatoolz <luatoolz@somahorse.com>
2024-09-11 01:46:08 +0300

    refactoring: now compatible with t.exporter

--- lua/t/format/bson.lua
+++ lua/t/format/bson.lua
@@ -1,14 +1,18 @@
-local driver = require "mongo"
 require "meta"
+local t=t or require "t"
+local tex=t.exporter
+local driver = require "mongo"
 
 local getmetatable = debug.getmetatable or getmetatable
 local setmetatable = debug.setmetatable or setmetatable
 
-local atom = {
-  ["number"]=true,
-  ["boolean"]=true,
-  ["string"]=true,
-}
+local function add(self)
+  if type(self)~='table' or getmetatable(self) then return self end
+  if type(self[1])~='nil' then self.__array=true end
+  local rv={}
+  for k,v in pairs(self) do rv[k]=add(v) end
+  return rv
+end
 
 local function clear(self)
   if type(self)~='table' then return self end
@@ -18,53 +22,35 @@ local function clear(self)
   return self
 end
 
-local function __bson(x)
-  local mt = getmetatable(x or {}) or {}
-  local to = mt.__toBSON or mt.__tobson
-  if type(to)=='function' then return to(x) end
-end
-
 local function is_mongo(x)
   return type(x)=='userdata' and (driver.type(x) or ''):startswith('mongo.')
+end
+
+local object = driver.BSON({})
+if type(object)~='nil' then
+  local mt = getmetatable(object)
+  assert(type(mt)=='table')
+  local __value = mt.value
+  assert(type(mt.value)=='function')
+  mt.value = function(self, handler) return clear(__value(self, handler)) end
+  mt.__call = function(self, handler) return self:value(handler) end
+
+  local __bson=driver.BSON
+  driver.BSON=function(self) return __bson(add(self)) end
 end
 
 return setmetatable({
   null=driver.Null,
   encode=function(x) return driver.BSON(x) end,
-  decode=function(x) if is_mongo(x) then return clear(x:value()) end end,
+  decode=function(x, handler) if is_mongo(x) then return clear(x:value(handler)) end end,
   type=driver.type,
   mongo=is_mongo,
-  tobson=function(x)
-    local mt = getmetatable(x or {}) or {}
-    return mt.__toBSON or mt.__tobson
-  end,
 },{
-  __call=function(self, x)
-    if type(x)=='function' or type(x)=='thread' or type(x)=='CFunction' then x=tostring(x) end
-    if atom[type(x)] then return x end
+  __call=function(self, x, skip)
     if rawequal(self.null, x) or type(x)=='nil' then return self.null end
-    if self.mongo(x) then return x end
-    if type(x)=='userdata' then
-      local mt = getmetatable(x or {}) or {}
-      local to = mt.__toBSON or mt.__tobson
-      if type(to)=='function' then
-        x=__bson(x)
-        return self.tobson(x) and self(x) or self.encode(x)
-      end
-      return self.encode(x)
-    end
-    if type(x)=='table' then
-      if not getmetatable(x) then return self.encode(x) end
-      local mt = getmetatable(x or {}) or {}
-      local to = mt.__toBSON or mt.__tobson
-      if type(to)=='function' then
-        x=__bson(x)
-        return self.tobson(x) and self(x) or self.encode(x)
-      end
-      local rv={}
-      for k,v in pairs(x) do rv[k]=self(v) end
-      return self.encode(rv)
-    end
-    error('unknown type' .. type(x))
+--    if is.string(x) then return assert(self.encode(x)) end
+--    if is.atom(x) then return x end
+--    if is.atom(x) then return assert(self.encode(x)) end
+    return assert(self.encode(tex(x, true)))
   end,
 })
