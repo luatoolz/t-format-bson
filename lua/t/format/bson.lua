4a2e11ea1ad128558c20d17f0b291b7fc3056987
luatoolz <luatoolz@somahorse.com>
2024-09-28 16:53:25 +0300

    fix: minor fixes to prevent auto-insulate

--- lua/t/format/bson.lua
+++ lua/t/format/bson.lua
@@ -1,10 +1,10 @@
 local t=t or require "t"
 local is=t.is
 local driver = require "mongo"
-require "t/patch.d/bson"
+local BSON = require "t/patch.d/bson"
 
 return setmetatable({
-  encode=driver.BSON,
+  encode=BSON,
   decode=function(x, handler) if is.bson(x) then return x(handler) end; return x end,
   type=driver.type,
 },{
