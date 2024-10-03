ae4600fd85387cf7ed3ef60a4d1dffef39f6af69
luatoolz <luatoolz@somahorse.com>
2024-10-03 07:29:35 +0300

    fix mt on demand, revert from patch.d

---
+++ lua/t/format/bson/encode.lua
@@ -0,0 +1,25 @@
+local t=t or require "t"
+local export=assert(t.exporter, "no t.exporter")
+local driver = assert(require "mongo")
+local add=function(x) return export(x, true) end
+local clear=function(x) return export(x, false) end
+
+local object = driver.BSON({})
+local mt = assert(getmetatable(object), "no BSON object")
+
+if not mt.__export then
+  local __value = mt.value
+  assert(type(mt.value)=='function', "no BSON.mt.value")
+  mt.value = function(self, handler) return clear(__value(self, handler)) end
+  mt.__call = function(self, handler) return self:value(handler) end
+  mt.__export = function(self) return self:value() end
+
+  local __bson=driver.BSON
+  driver.BSON=function(self)
+    assert(self, "driver.BSON: nil argument");
+    if type(self)=='table' then self=add(self) end
+    return self and __bson(self)
+  end
+end
+
+return driver.BSON
---
+++ lua/t/format/bson/init.lua
@@ -0,0 +1,14 @@
+local t=t or require "t"
+local is=t.is
+local pkg = t.pkg(...)
+local driver = require "mongo"
+
+return setmetatable({
+  encode=pkg.encode,
+  decode=function(x, handler) if is.bson(x) then return x(handler) end; return end,
+  type=driver.type,
+},{
+  __call=function(self, x) return x and self.encode(x) end,
+  __mod=function(self, it) return is.bson(it) end,
+  __tostring=function(self) return t.type(self) end,
+})
